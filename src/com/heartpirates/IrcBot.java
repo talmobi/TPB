90410c67be1ddbba5f7801b8816736bdcdb661d8
unknown <talmo.christian@gmail.com>
2014-02-21 06:56:58 +0200

    basic irc bot done - connecting, listening, parting

--- src/com/heartpirates/IrcBot.java
+++ src/com/heartpirates/IrcBot.java
@@ -14,7 +14,7 @@ import java.net.UnknownHostException;
  * http://help.twitch.tv/customer/portal/articles/1302780-twitch-irc
  */
 
-public class IrcBot {
+public class IrcBot implements Runnable {
 
 	BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
 
@@ -31,35 +31,107 @@ public class IrcBot {
 	String nick = "mooglebones";
 
 	// twitch oauth key - www.twitchapps.com/tmi
-	String oauth = "oauth:33a21tw2ih4lprqpoqfazzv2evzp8zl"; // get you
+	String pass = "oauth:33a21tw2ih4lprqpoqfazzv2evzp8zl";
 
-	public IrcBot() throws UnknownHostException, IOException {
-		connect();
-	}
+	public IrcBot() {
+		new Thread(this).start();
 
-	private void connect() throws UnknownHostException, IOException {
+		BufferedReader inputReader = new BufferedReader(new InputStreamReader(
+				System.in));
 
-		// connect to the irc server
-		socket = new Socket(server, port);
-
-		// create helper classes for reading/writing
-		BufferedReader reader = new BufferedReader(new InputStreamReader(
-				socket.getInputStream()));
-		writer = new BufferedWriter(new OutputStreamWriter(
-				socket.getOutputStream()));
-
-		// login
-		writer.write("PASS " + oauth + "\r\n");
-		writer.write("NICK " + nick + "\r\n");
-
-		String readLine = null;
-		while ((readLine = reader.readLine()) != null) { // blocks
-			handleMessage(readLine);
+		String inLine = null;
+		try {
+			while ((inLine = inputReader.readLine()) != null) {
+				send(inLine);
+			}
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
 		}
 	}
 
-	private void handleMessage(String msg) {
+	@Override
+	public void run() {
+		// connect to the IRC server
+		try {
+			print("connecting... ");
+			socket = new Socket(server, port);
 
+			// create helper classes for reading/writing
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+					socket.getInputStream()));
+			writer = new BufferedWriter(new OutputStreamWriter(
+					socket.getOutputStream()));
+
+			// login
+			writer.write("PASS " + pass + "\r\n");
+			writer.write("NICK " + nick + "\r\n");
+			writer.write("USER " + "guest :" + nick + "\r\n");
+			writer.flush();
+
+			print("logging in... ");
+			String line = null;
+			while ((line = reader.readLine()) != null) { // blocks
+				if (line.indexOf("004") >= 0) {
+					print("Success!\r\n");
+					break;
+				}
+			}
+
+			// auto join channel
+			writer.write("JOIN " + channel + "\r\n");
+			writer.flush();
+
+			// read messages
+			while ((line = reader.readLine()) != null) { // blocks
+				if (line.startsWith("PING ")) {
+					print("PING ");
+					writer.write("PONG " + line.substring(5) + "\r\n");
+					writer.flush();
+					print("PONG\r\n");
+				} else {
+					handleMessage(line);
+				}
+			}
+
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	class IRCMessage {
+		String nick;
+		String user;
+		String host;
+		String command;
+		String channel;
+		String text;
+
+		@Override
+		public String toString() {
+			return nick + " " + user + " " + host + " " + command + " "
+					+ channel + " " + text;
+		}
+	}
+
+	private void handleMessage(String line) {
+
+		String[] tokens = line.split(" ", 4);
+
+		if (tokens[1].equals("PRIVMSG")) {
+			String nick = "";
+			if (line.indexOf('!') >= 0)
+				nick = line.substring(1, line.indexOf('!'));
+			String text = "";
+			if (tokens.length >= 3)
+				text = tokens[3].substring(1);
+
+			System.out.println(nick + ": " + text);
+		} else {
+			System.out.println(line);
+		}
 	}
 
 	public void print(String str) throws IOException {
@@ -77,7 +149,9 @@ public class IrcBot {
 	}
 
 	public static void main(String[] args) {
+		new IrcBot();
 
+		System.out.println("Closing.");
 	}
 
 }
